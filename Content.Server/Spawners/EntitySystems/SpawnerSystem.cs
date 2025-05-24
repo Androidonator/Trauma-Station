f6f04e8092151cd6e6be92d5b0a485c9c1ee19b7
Tayrtahn <tayrtahn@gmail.com>
2025-05-24 13:35:17 -0400

    Convert `TimedSpawnerComponent` to use an Update loop (#37785)
    Convert TimedSpawnerComponent to use an Update loop

--- Content.Server/Spawners/EntitySystems/SpawnerSystem.cs
+++ Content.Server/Spawners/EntitySystems/SpawnerSystem.cs
@@ -1,28 +1,44 @@
 // <Trauma>
 using Content.Trauma.Common.Spawners;
 // </Trauma>
-using System.Threading;
 using Content.Server.Spawners.Components;
 using Robust.Shared.Random;
+using Robust.Shared.Timing;
 
 namespace Content.Server.Spawners.EntitySystems;
 
 public sealed class SpawnerSystem : EntitySystem
 {
+    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
 
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<TimedSpawnerComponent, ComponentInit>(OnSpawnerInit);
-        SubscribeLocalEvent<TimedSpawnerComponent, ComponentShutdown>(OnTimedSpawnerShutdown);
+
+        SubscribeLocalEvent<TimedSpawnerComponent, MapInitEvent>(OnMapInit);
     }
 
-    private void OnSpawnerInit(EntityUid uid, TimedSpawnerComponent component, ComponentInit args)
+    public override void Update(float frameTime)
     {
-        component.TokenSource?.Cancel();
-        component.TokenSource = new CancellationTokenSource();
-        uid.SpawnRepeatingTimer(TimeSpan.FromSeconds(component.IntervalSeconds), () => OnTimerFired(uid, component), component.TokenSource.Token);
+        base.Update(frameTime);
+
+        var curTime = _timing.CurTime;
+        var query = EntityQueryEnumerator<TimedSpawnerComponent>();
+        while (query.MoveNext(out var uid, out var timedSpawner))
+        {
+            if (timedSpawner.NextFire > curTime)
+                continue;
+
+            OnTimerFired(uid, timedSpawner);
+
+            timedSpawner.NextFire += timedSpawner.IntervalSeconds;
+        }
+    }
+
+    private void OnMapInit(Entity<TimedSpawnerComponent> ent, ref MapInitEvent args)
+    {
+        ent.Comp.NextFire = _timing.CurTime + ent.Comp.IntervalSeconds;
     }
 
     private void OnTimerFired(EntityUid uid, TimedSpawnerComponent component)
@@ -42,10 +58,5 @@ public sealed class SpawnerSystem : EntitySystem
             RaiseLocalEvent(uid, ref ev);
             // </Trauma>
         }
-    }
-
-    private void OnTimedSpawnerShutdown(EntityUid uid, TimedSpawnerComponent component, ComponentShutdown args)
-    {
-        component.TokenSource?.Cancel();
     }
 }
