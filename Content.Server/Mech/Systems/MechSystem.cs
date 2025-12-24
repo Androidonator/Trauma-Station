80a443d8a989b0e2a5c55ef9453219ffd66fa7d4
TVK-04 <114073746+TVK-04@users.noreply.github.com>
2025-12-24 18:19:08 +0100

    Fixed Voice Mask and Ripley APU interaction (#42023)
    Would show player's real identity instead of assumed identity
    
    Co-authored-by: TVK-04 <>

--- Content.Server/Mech/Systems/MechSystem.cs
+++ Content.Server/Mech/Systems/MechSystem.cs
@@ -1,5 +1,6 @@
-using Content.Shared.Emp; // Goobstation
-using System.Linq;
+// <Trauma>
+using Content.Shared.Emp;
+// </Trauma>
 using Content.Server.Atmos.EntitySystems;
 using Content.Server.Body.Systems;
 using Content.Server.Mech.Components;
@@ -7,6 +8,7 @@ using Content.Shared.ActionBlocker;
 using Content.Shared.Damage.Systems;
 using Content.Shared.DoAfter;
 using Content.Goobstation.Maths.FixedPoint;
+using Content.Shared.IdentityManagement;
 using Content.Shared.Interaction;
 using Content.Shared.Mech;
 using Content.Shared.Mech.Components;
@@ -26,6 +28,7 @@ using Robust.Server.GameObjects;
 using Robust.Shared.Containers;
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
+using System.Linq;
 
 namespace Content.Server.Mech.Systems;
 
@@ -223,7 +226,7 @@ public sealed partial class MechSystem : SharedMechSystem
                     {
                         BreakOnMove = true,
                     };
-                    _popup.PopupEntity(Loc.GetString("mech-eject-pilot-alert", ("item", uid), ("user", args.User)), uid, PopupType.Large);
+                    _popup.PopupEntity(Loc.GetString("mech-eject-pilot-alert", ("item", uid), ("user", Identity.Entity(args.User, EntityManager))), uid, PopupType.Large);
 
                     _doAfter.TryStartDoAfter(doAfterEventArgs);
                 }
@@ -240,7 +243,7 @@ public sealed partial class MechSystem : SharedMechSystem
         if (_whitelistSystem.IsWhitelistFail(component.PilotWhitelist, args.User)
             || _whitelistSystem.IsWhitelistPass(component.PilotBlacklist, args.User)) // Goobstation Change
         {
-            _popup.PopupEntity(Loc.GetString("mech-no-enter", ("item", uid)), args.User);
+            _popup.PopupEntity(Loc.GetString("mech-no-enter", ("item", uid)), Identity.Entity(args.User, EntityManager));
             return;
         }
 
